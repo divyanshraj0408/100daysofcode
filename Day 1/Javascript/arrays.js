ec3e611da3de08bcfd08a6cb89eb2fa55a0678bf
Divyansh <divyanshraj0408@gmail.com>
2022-01-02 23:53:40 +0530

    made arrMw

--- Day 1/Javascript/arrays.js
+++ Day 1/Javascript/arrays.js
@@ -3,3 +3,5 @@ let arr = ["this", "is", "an", "array"]
     // arrays are 0 indexed
 console.log(arr[0])
 console.log(arr.length) //length of the array is counted from 1
+
+let arrMe = ["Divyansh", "17", "studies at Sant Longowal Institute of Engineering and technology"]
