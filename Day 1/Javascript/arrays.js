b0ad2ca59b2f32ddd478ce5f2bb861039e3e7121
Divyansh <divyanshraj0408@gmail.com>
2022-01-02 23:31:52 +0530

    Day1 code

---
+++ Day 1/Javascript/arrays.js
@@ -0,0 +1,5 @@
+//Arrays are ordered list of items
+let arr = ["this", "is", "an", "array"]
+    // arrays are 0 indexed
+console.log(arr[0])
+console.log(arr.length) //length of the array is counted from 1
