456a5513b6ec8101a7396b7de6a1d61e5fe65647
Divyansh <divyanshraj0408@gmail.com>
2022-01-03 23:44:07 +0530

    Day 2 of 100DaysOfCode

---
+++ Day 2/Javascript/Arrays/arrays.js
@@ -0,0 +1,16 @@
+let cards = [7, 4]
+cards.push(6)
+console.log(cards)
+
+//Exercise : Push  the newMessage to the messages array,and then log out the array.
+let message = [
+    "hey, how are you?",
+    "I am great, thankyou! How about you?",
+    "All good. Been working on 100DaysOfCode today"
+]
+
+let newMessage = "That's Great!"
+message.push(newMessage)
+console.log(message)
+message.pop() //to remove the last item in the arrays
+console.log(message)
---
+++ Day 2/Javascript/counting/index.js
@@ -0,0 +1,4 @@
+//for loops
+for (let count = 1; count < 11; count += 1) {
+    console.log(count)
+}
