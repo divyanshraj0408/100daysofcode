a8a0e1a1bfc32ab0a42b67fb344fdaff3034f7ae
Divyansh <divyanshraj0408@gmail.com>
2022-01-28 23:33:25 +0530

    Day 27 of 100DaysOfCode

---
+++ Day 027/Java/perfectsqr.java
@@ -0,0 +1,33 @@
+// Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+// Follow up: Do not use any built-in library function such as sqrt.
+
+ 
+
+// Example 1:
+
+// Input: num = 16
+// Output: true
+// Example 2:
+
+// Input: num = 14
+// Output: false
+public class perfectsqr {
+    public boolean isPerfectSquare(int num) {
+        long start =1;
+        long end = num;
+         while(end>=start){
+             long mid = start +  (end-start)/2;
+             if(mid*mid==num){
+                 return true;
+             }
+             else if(mid*mid>num){
+                 end = mid-1;
+             }
+             else{
+                 start = mid+1;
+             }
+         }
+         return false;  
+     }
+}
