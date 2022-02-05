8f394abd65385f22a3c93fb2c44fab3a8095ac33
Divyansh <divyanshraj0408@gmail.com>
2022-02-06 00:18:56 +0530

    Day 35 of 100DaysOfCode

---
+++ Day 035/java/reverseString.java
@@ -0,0 +1,15 @@
+public class reverseString {
+    public void reverseStrings(char[] s) {
+        int i =0;
+      int j= s.length-1;
+        
+        while(i<j){
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]= temp;
+            
+            i++;
+            j--;
+        }
+    }
+}
