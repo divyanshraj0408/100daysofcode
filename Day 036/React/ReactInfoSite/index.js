163cf0e995b0d8c2d127a7cdf085c4f15261e8a4
Divyansh <divyanshraj0408@gmail.com>
2022-02-06 23:48:09 +0530

    Day 36 of 100DaysOfCode

---
+++ Day 036/React/ReactInfoSite/index.js
@@ -0,0 +1,7 @@
+ReactDOM.render( < h1 > Hey everyone < /h1>, document.getElementById("root"))
+        ReactDOM.render( < ul >
+            <
+            li > List one < /li>  <
+            li > List two < /li>  < /
+            ul > , document.getElementById("root")
+        )
