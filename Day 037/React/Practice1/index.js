856529f795e17fa02fe2082f6abe738db9b5eb39
Divyansh <divyanshraj0408@gmail.com>
2022-02-07 23:16:51 +0530

    Day 37 of 100DaysOfCode

---
+++ Day 037/React/Practice1/index.js
@@ -0,0 +1,8 @@
+// const h1Element = document.createElement("h1")
+
+// h1Element.textContent = "This is a element"
+// h1Element.className = "header"
+// document.getElementById("root").append(h1Element)
+
+// All of the above can be done simply by using react coz it is declarative 
+ReactDOM.render( < h1 > This is JSX < /h1>, document.getElementById("root"))
