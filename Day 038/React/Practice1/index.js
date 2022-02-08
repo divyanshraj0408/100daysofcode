a34a94f879d1d270794d77203125d4578e9f21a5
Divyansh <divyanshraj0408@gmail.com>
2022-02-08 21:19:59 +0530

    Day 38 of 100DaysOfCode

---
+++ Day 038/React/Practice1/index.js
@@ -0,0 +1,13 @@
+const page = ( <
+    nav >
+    <
+    h1 > Website < /h1> <
+    ul >
+    <
+    li > Pricing < /li> <li> About </li > < li > Contact < /li> < /
+    ul > <
+    /nav>
+);
+
+let rootEl = document.getElementById("root");
+ReactDOM.render(page, rootEl);
