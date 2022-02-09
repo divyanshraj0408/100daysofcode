fe69a27f088081655b9efd9338d434a7559e7a69
Divyansh <divyanshraj0408@gmail.com>
2022-02-09 22:49:49 +0530

    Day 39 of 100DaysOfCode

---
+++ Day 039/React/practice1/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo.svg";
+import "./App.css";
+
+function SomeName(){
+  return(
+  <div>
+    <img src="./logo-og.png" />
+    <h1> Fun facts about React </h1>
+    <ul>
+      <li> It was released in 2013 </li>
+      <li> Was originally created by Jordan Walke </li>
+      <li> Has well over 100 k stars on github.com </li>
+      <li> Is maintained by Facebook </li>
+    </ul>
+  </div>
+  )
+}
+ReactDOM.render(<SomeName/>, document.getElementById("root"));
+
+export default App;
