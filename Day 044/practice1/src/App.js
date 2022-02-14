1ee49de71e8d442480a87e75799faf3348dce229
Divyansh Raj <divyanshraj0408@gmail.com>
2022-02-15 00:00:52 +0530

    Day 43 of 100DaysOfCode

---
+++ Day 044/practice1/src/App.js
@@ -0,0 +1,16 @@
+import "./components/Main";
+import "./components/Navbar";
+import "./App.css";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ Day 044/practice2/src/components/MainContent.js
@@ -0,0 +1,10 @@
+function MainContent(){
+    return(
+        <div className = "MainContent">
+            <h1>Lorem, ipsum.</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <small>Lorem, ipsum.</small>
+        </div>
+    )
+}
+export default MainContent;
