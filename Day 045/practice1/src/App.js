9a74c0e1590241205279b692424328b425eb2a60
Divyansh Raj <divyanshraj0408@gmail.com>
2022-02-15 23:28:52 +0530

    Day 45 of 100DaysOfCode

---
+++ Day 045/practice1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import "./components/Image"
+import "./components/MainContent"
+import MainContent from "./components/MainContent"
+import Image from "./components/Image"
+
+function App() {
+  return (
+    <div className="App">
+      <Image/>
+      <MainContent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day 045/practice1/src/components/MainContent.js
@@ -0,0 +1,27 @@
+function MainContent() {
+  return (
+    <div className="MainContent">
+      <h2>Divyansh Raj</h2>
+      <p>FrontEnd Developer</p>
+      <small>
+        <a href="http://linkfree.eddiehub.org/divyanshraj0408" target = "_blank">
+          divyanshraj.website
+        </a>
+      </small>
+      <br />
+      <button className= "main--btn" >Email</button>
+      <h4>About</h4>
+      <p>
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+      <h4>Intrests</h4>
+      <p>
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
+}
+export default MainContent;
