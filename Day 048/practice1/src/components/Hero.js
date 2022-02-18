6354a8449ad26f8c6e5832541b5c5a0b025a69b5
Divyansh Raj <divyanshraj0408@gmail.com>
2022-02-18 22:42:33 +0530

    Day 48 of 100DaysOfCode

---
+++ Day 048/practice1/src/components/Hero.js
@@ -0,0 +1,9 @@
+import Group77 from "../images/Group77.png"
+function Hero(){
+    return(
+        <div className="Hero-component">
+            <img src={Group77} alt="" />
+        </div>
+    )
+}
+export default Hero;
