616555fbb687d3de66e35627458e6b7762961086
Divyansh Raj <divyanshraj0408@gmail.com>
2022-02-22 21:42:05 +0530

    Day 52 of 100DaysOfCode

---
+++ Day 052/practice1/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Card from './components/card';
+import card1 from "./images/Zaferes.png"
+
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <Hero/>
+    <Card 
+    img = {card1}
+     rating ={5.0}
+    reviewCount= {6}
+     country= "USA"
+     title ="Life Lessons with Katie Zaferes"
+     price ={136}/>
+    </div>
+  );
+}
+
+export default App;
