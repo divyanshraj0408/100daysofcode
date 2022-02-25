004be02a1857d845daf165dc93fd23c19faaab5b
Divyansh Raj <divyanshraj0408@gmail.com>
2022-02-25 21:21:30 +0530

    Day 55 of 100DaysOfCode

---
+++ Day 055/practice1/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/card";
+import data from "./data";
+import Zaferes from "./images/Zaferes.png"
+
+function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+        openSpots={item.openSpots}
+      />
+    );
+  });
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">{cards}</section>
+    </div>
+  );
+}
+
+export default App;
