670a37df096a330fd9123e4b318e4c1c1dbaa573
Divyansh Raj <divyanshraj0408@gmail.com>
2022-03-03 23:48:19 +0530

    Day 61 of 100DaysOfCode

---
+++ Day 061/practice1/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Form from './components/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Form/>
+    </div>
+  );
+}
+
+export default App;
+
